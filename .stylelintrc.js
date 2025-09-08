e302929355e97fff2124c0ea1eeb84e5ff53f22e
Robert Wagner <rwwagner90@gmail.com>
2025-09-08 09:49:59 -0400

    Fix lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,9 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
   rules: {
+    'declaration-block-no-redundant-longhand-properties': null,
+    'no-descending-specificity': null,
     'selector-class-pattern': null,
+    'value-no-vendor-prefix': null,
   },
 };

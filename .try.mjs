c591480c96be9b4c3f1901f92ed8faeff0963d77
BobrImperator <bartholomew.dudzik@gmail.com>
2026-04-16 16:40:06 +0200

    chore: add ember v4 scenarios

--- .try.mjs
+++ .try.mjs
@@ -27,6 +27,32 @@ export default {
   packageManager: 'pnpm',
   scenarios: [
     {
+      name: 'ember-lts-4.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~4.12.0',
+          ...compatDeps,
+        },
+      },
+      env: {
+        ENABLE_COMPAT_BUILD: true,
+      },
+      files: compatFiles,
+    },
+    {
+      name: 'ember-lts-5.4',
+      npm: {
+        devDependencies: {
+          'ember-source': '~5.4.0',
+          ...compatDeps,
+        },
+      },
+      env: {
+        ENABLE_COMPAT_BUILD: true,
+      },
+      files: compatFiles,
+    },
+    {
       name: 'ember-lts-5.8',
       npm: {
         devDependencies: {
